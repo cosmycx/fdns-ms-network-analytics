e0a30d279f635224b9caedda98612f4670a55bf8
Cosmin Potocean <cosmycx@gmail.com>
2018-10-04 11:57:39 -0400

    image

--- app.py
+++ app.py
@@ -11,14 +11,12 @@ app = Flask(__name__)
 def root_dir():  # pragma: no cover
     return os.path.abspath(os.path.dirname(__file__))
 
-
 def get_file(filename):  # pragma: no cover
     try:
         src = os.path.join(root_dir(), filename)
         return open(src).read()
     except IOError as exc:
         return str(exc)
-
 
 # ------------------------------------------------
 #               / 
