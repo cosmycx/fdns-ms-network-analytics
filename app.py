dfb75ef713e5315ccfd0914e17bb529fba7da547
Cosmin Potocean <cosmycx@gmail.com>
2018-10-04 10:02:55 -0400

    dynamic location

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import jsonify, request
 
 import os.path
 
-
 import osmnx as ox
 from networkx.readwrite import json_graph
 
@@ -20,7 +19,6 @@ def get_file(filename):  # pragma: no cover
     except IOError as exc:
         return str(exc)
 
-
 # ------------------------------------------------
 #               / 
 # ------------------------------------------------
@@ -36,14 +34,14 @@ def graph_from_address():
 
     values = request.get_json()
     location = values.get('location')
-    network_mode = values.get('network_mode')
+    network_type = values.get('network_type')
     print(location)
-    print(network_mode)
+    print(network_type)
 
     if location is None:
         return "Error, please supply a valid location", 400
-    if network_mode is None:
-        return "Error, please supply a valid network_mode", 400
+    if network_type is None:
+        return "Error, please supply a valid network_type", 400
     return 'ok', 200
 
 # ------------------------------------------------
@@ -54,24 +52,24 @@ def graph_from_point():
 
     values = request.get_json()
     location = values.get('location')
-    network_mode = values.get('network_mode')
+    network_type = values.get('network_type')
     print(location)
-    print(network_mode)
+    print(network_type)
 
-    G = ox.graph_from_place('Manhattan, New York, USA', network_type='drive')
+    G = ox.graph_from_place(location, network_type=network_type)
 
     if location is None:
         return "Error, please supply a valid location", 400
-    if network_mode is None:
-        return "Error, please supply a valid network_mode", 400
+    if network_type is None:
+        return "Error, please supply a valid network_type", 400
 
-    ox.save_graphml(G, filename="testgraph.graphml", folder="/app")
+    ox.save_graphml(G, filename="graphfile.graphml", folder="/app")
 
     #jdata = json_graph.tree_data(G,root=1)
     #graphml_json = json_graph.tree_graph(jdata)
     #return jsonify(graphml_json), 200
     # return 'ok', 200
-    content = get_file('testgraph.graphml')
+    content = get_file('graphfile.graphml')
     return Response(content, mimetype="application/xml")
 
 
